891c6d49e2e3b28d4640bba90685e2d0ef96d8a4
wiaamm <wiaamm@checkpoint.com>
2026-01-15 12:15:14 +0200

    fix wrong type

--- attachments/kong/plugins/open-appsec-waf-kong-plugin/lua_attachment_wrapper.c
+++ attachments/kong/plugins/open-appsec-waf-kong-plugin/lua_attachment_wrapper.c
@@ -407,7 +407,7 @@ static int lua_send_body(lua_State *L) {
     }
 
     if (body_len <= 8 * 1024) {
-        HttpBody http_chunks;
+        NanoHttpBody http_chunks;
         http_chunks.bodies_count = 1;
         
         nano_str_t chunk;
@@ -443,7 +443,7 @@ static int lua_send_body(lua_State *L) {
         num_chunks = 10000;
     }
 
-    HttpBody http_chunks;
+    NanoHttpBody http_chunks;
     http_chunks.bodies_count = num_chunks;
 
     http_chunks.data = (nano_str_t*)malloc(num_chunks * sizeof(nano_str_t));
