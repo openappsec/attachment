dd5433b16a4f408d671d4bbdc9d2a69fd934a536
danielya <danielya@checkpoint.com>
2026-05-05 16:02:56 +0300

    Adjusted for changes.

--- attachments/nano_attachment/nano_attachment.c
+++ attachments/nano_attachment/nano_attachment.c
@@ -689,3 +689,21 @@ freeCompressedBody(NanoAttachment *attachment, HttpSessionData *session_data, Na
 {
     nano_free_compressed_body(attachment, bodies, session_data);
 }
+
+uint32_t
+GetRequestProcessingTimeout(NanoAttachment *attachment)
+{
+    if (attachment == NULL) {
+        return 3000;
+    }
+    return attachment->req_max_proccessing_ms_time;
+}
+
+uint32_t
+GetResponseProcessingTimeout(NanoAttachment *attachment)
+{
+    if (attachment == NULL) {
+        return 3000;
+    }
+    return attachment->res_max_proccessing_ms_time;
+}
