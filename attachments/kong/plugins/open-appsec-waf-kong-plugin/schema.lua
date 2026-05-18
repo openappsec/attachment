6f017c82577f3a772f5765381b96a49a9c735a9f
wiaamm <wiaamm@checkpoint.com>
2026-05-18 14:07:56 +0300

    remove cloudguard name

--- attachments/kong/plugins/open-appsec-waf-kong-plugin/schema.lua
+++ attachments/kong/plugins/open-appsec-waf-kong-plugin/schema.lua
@@ -1,7 +1,7 @@
 local typedefs = require "kong.db.schema.typedefs"
 
 return {
-  name = "cloudguard-waf-kong-plugin",
+  name = "open-appsec-waf-kong-plugin",
   fields = {
     { consumer = typedefs.no_consumer },                -- required for Konnect compatibility
     { protocols = typedefs.protocols_http },            -- required so Konnect knows when to allow this plugin
